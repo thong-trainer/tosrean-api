2612f20e008aefbd7afd347f6b206a87402fddac
thong-trainer <leykamthong@Leys-MacBook-Pro.local>
2018-06-02 11:22:00 +0700

    updated class rout

--- routes/api/class.js
+++ routes/api/class.js
@@ -54,7 +54,7 @@ Paramas: class_id as String
 Token: required
 Method: POST
 */
-router.route('/add-remove-student/:id').put(ClassController.addOrRemoveStudent);
+router.route('/student/:id').put(ClassController.addOrRemoveStudent);
 
 /*
 NOTE: Remove Class by Id
